a3eea3cb11a8a0cefb2f1d433fd8df1411d64b5e
Cristi Pele <cristian.pele@adonissoft.com>
2022-08-03 18:20:09 +0300

    sign up page - design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import SignIn from './components/SignIn';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#ECAB14',
+        contrastText: '#FFFFFF'
+      }
+    },
+  });
+
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <SignIn />    
-    </div>
+    </ThemeProvider>
   );
 }
 
--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,10 +1,62 @@
 import React, {  } from 'react';
+import './styles.css'
+import logo from '../../images/logo.png'
+import { TextField, InputAdornment, Button, Checkbox, FormControlLabel } from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import SecurityIcon from '@mui/icons-material/Security';
 
 const SignIn = (props) => {
 
   return (
-    <div>
-      This is login page
+    <div className='sign_up'>
+      <div className='sign_up_body'>
+        <img alt="" className="sign_up_logo" src={logo} />
+        <div className='welcome_text'>
+          Welcome to <span className='welcome_text_bold'>MyINS Metrics <br /> Dashboard</span>, please Log In below.
+        </div>
+        <div className='sign_up_form'>
+          <TextField
+            autoFocus
+            variant='outlined'
+            placeholder="type username"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <AccountCircleIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+          <TextField
+            variant='outlined'
+            placeholder="type password"
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SecurityIcon />
+                </InputAdornment>
+              ),
+            }}
+          />
+          <Button
+            variant='contained'
+          >
+            Log In
+          </Button>
+          <FormControlLabel
+            className='checkbox_remember_me'
+            label="Remember me"
+            control={
+              <Checkbox />
+            }
+          />
+        </div>
+      </div>
+      <div className='sign_up_footer'>
+        <p>MyINS</p>
+        <p>Copyright &copy;2022</p>
+        <p>Squid40</p>
+      </div>
     </div>
   )
 };

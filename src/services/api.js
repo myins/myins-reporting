bd3fb525e51ab73d7cbb86a8fdd32984bc622e4e
Cristi Pele <cristian.pele@adonissoft.com>
2022-09-02 17:38:20 +0300

    fix https

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://myinsn-backend-dev-2011842023.us-west-2.elb.amazonaws.com/'
+  baseURL: 'https://myinsn-backend-dev-2011842023.us-west-2.elb.amazonaws.com/'
 });

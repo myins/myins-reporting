ca72123eb2cc5c71bb54ee88310f5e67556ce859
Cristi Pele <cristian.pele@adonissoft.com>
2022-09-12 11:42:33 +0300

    change base url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/',
+  baseURL: 'https://prod-api.myins.co.uk/',
 });
 
 export const setBearerToken = (user) => {

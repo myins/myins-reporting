87bafd153b0c2f820c46ae6a6bd8d3b7ac4cc2fd
Cristi Pele <cristian.pele@adonissoft.com>
2022-09-23 16:39:37 +0300

    change login request

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,5 +1,5 @@
 import { axiosInstance } from './api';
 
 export const login = async (body) => {
-  return axiosInstance.post('/auth/login', body);
+  return axiosInstance.post('/auth/login/admin', body);
 }
